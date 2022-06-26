3badd8cffe4d2f7727565e6e49ce215c26d69bae
Alex Masterov <alex.masterow@gmail.com>
2022-06-26 13:14:47 +0300

    Fix Д9

--- src/view/trip/trip-info-main-view.js
+++ src/view/trip/trip-info-main-view.js
@@ -2,19 +2,21 @@ import AbstractView from '@framework/view/abstract-view';
 
 import { formatEventDate } from '@util/date';
 
+const MAX_DESTINATION_SHOW = 3;
+
+const formatDestinationNames = (names) => names.length > MAX_DESTINATION_SHOW
+  ? `${names.at(0)} — ... — ${names.at(-1)}`
+  : `${names.join(' — ')}`;
+
+const formatTripDates = ({ dateStart, dateEnd }) =>
+  `${formatEventDate(dateStart)} — ${formatEventDate(dateEnd)}`;
+
 const createViewTemplate = () => (
   `<div class="trip-info__main">
     <h1 class="trip-info__title"></h1>
     <p class="trip-info__dates"></p>
   </div>`
 );
-
-const formatDestinationNames = (names) => names.length > 3
-  ? `${names.at(0)} — ... — ${names.at(-1)}`
-  : `${names.join(' — ')}`;
-
-const formatTripDates = ({ dateStart, dateEnd }) =>
-  `${formatEventDate(dateStart)} — ${formatEventDate(dateEnd)}`;
 
 class TripInfoMainView extends AbstractView {
   get template() {
